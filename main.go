e66d2eb744d3d7b9e9490315495e311ad5b6bf10
Leo <49914615+SnowJuli@users.noreply.github.com>
2021-06-24 13:59:22 +0200

    change structure

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "herupa/cmd"
+import (
+		"fmt"
+		"strings"
+		
+)
 
 func main() {
-	cmd.Execute()
+	var choice string
+
+	fmt.Println(`
+Herupa - A Hackintosh Manager
+-----------------------------
+- Backup your EFI (Backup)
+- Fetch Kexts (Fetch)
+- Gather new Opencorepkg (Opencore)
+- Transfer files between Backup and EFI (Transfer)
+-----------------------------
+Enter the Words in Brackets:`)
+
+	fmt.Scanf("%s", &choice)
+	var choice_lwr = strings.ToLower(choice)
+
+	switch {
+	case choice_lwr == "backup":
+		return
+	case choice_lwr == "fetch":
+		return
+	case choice_lwr == "opencore":
+		return
+	case choice_lwr == "transfer":
+		return
+	default:
+		fmt.Println("Please enter the right command.")
+	}
 }
